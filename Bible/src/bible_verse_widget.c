bf3fc75a0a975c72c8580e39478e85a01678f733
Godly T Alias <godlytalias@yahoo.co.in>
2016-06-12 00:46:10 +0530

    [English Bible] Fixed multiline layouts
    Signed-off-by: Godly T Alias <godlytalias@yahoo.co.in>

--- Bible/src/bible_verse_widget.c
+++ Bible/src/bible_verse_widget.c
@@ -39,9 +39,13 @@ _verse_widget_del_item(void *data, Evas_Object *obj, void *event_info)
 Evas_Object*
 _verse_widget_content_get_cb(void *data, Evas_Object *obj, const char *part)
 {
-	if (!strcmp(part, "elm.swallow.end"))
+	if (!strcmp(part, "elm.swallow.content"))
 	{
-		Evas_Object *ic_cancel = elm_button_add(obj);
+		char text[1024];
+		bible_verse_item *verse_item = (bible_verse_item*)data;
+		Evas_Object *layout = elm_layout_add(obj);
+		elm_layout_file_set(layout, verse_item->appdata->edj_path, "widget_verse_item_layout");
+		Evas_Object *ic_cancel = elm_button_add(layout);
 		elm_object_style_set(ic_cancel, "circle");
 		evas_object_size_hint_min_set(ic_cancel, ELM_SCALE_SIZE(32), ELM_SCALE_SIZE(32));
 		Evas_Object *icon = elm_icon_add(obj);
@@ -52,24 +56,13 @@ _verse_widget_content_get_cb(void *data, Evas_Object *obj, const char *part)
 		elm_object_content_set(ic_cancel, icon);
 		evas_object_data_set(ic_cancel, "genlist", obj);
 		evas_object_smart_callback_add(ic_cancel, "clicked", _verse_widget_del_item, data);
-		return ic_cancel;
-	}
-	else return NULL;
-}
-
-char*
-_verse_widget_text_get_cb(void *data, Evas_Object *obj, const char *part)
-{
-	char text[1024];
-	bible_verse_item *verse_item = (bible_verse_item*)data;
-	if (!strcmp(part, "elm.text"))
-	{
 		sprintf(text, "%s %d:%d", Books[verse_item->bookcount], verse_item->chaptercount, verse_item->versecount + 1);
-		return strdup(text);
-	}
-	else if (!strcmp(part, "elm.text.multiline"))
-	{
-		return strdup(verse_item->verse);
+		elm_layout_text_set(layout, "elm.text.reference", text);
+		elm_layout_text_set(layout, "elm.text.verse", verse_item->verse);
+		elm_layout_content_set(layout, "elm.icon", ic_cancel);
+		elm_layout_sizing_eval(layout);
+		evas_object_show(layout);
+		return layout;
 	}
 	else return NULL;
 }
@@ -98,11 +91,13 @@ _get_verse(void *data, int argc, char **argv, char **azColName)
 	Elm_Object_Item *it;
 	Evas_Object *genlist = (Evas_Object*)data;
 	Elm_Genlist_Item_Class *itc = (Elm_Genlist_Item_Class*)evas_object_data_get(genlist, "itc");
+	appdata_s *ad = (appdata_s*)evas_object_data_get(genlist, "appdata");
 	bible_verse_item *verse_item = (bible_verse_item*)malloc(sizeof(bible_verse_item));
 	verse_item->bookcount = atoi(argv[0]);
 	verse_item->chaptercount = atoi(argv[1]);
 	verse_item->versecount = atoi(argv[2]);
 	verse_item->verse = strdup(argv[3]);
+	verse_item->appdata = ad;
 	it = elm_genlist_item_append(genlist, itc, verse_item, NULL, ELM_GENLIST_ITEM_NONE, NULL, NULL);
 	verse_item->it = it;
 	return 0;
@@ -129,11 +124,12 @@ _verse_display_widget_list(appdata_s *ad)
 	elm_genlist_reorder_mode_set(genlist, EINA_TRUE);
 	evas_object_smart_callback_add(genlist, "moved", _genlist_item_reorder_cb, ad);
 	Elm_Genlist_Item_Class *itc = elm_genlist_item_class_new();
-	itc->item_style = "multiline";
-	itc->func.text_get = _verse_widget_text_get_cb;
+	itc->item_style = "full";
+	itc->func.text_get = NULL;
 	itc->func.content_get = _verse_widget_content_get_cb;
 	itc->func.del = _verse_widget_item_del_cb;
 	evas_object_data_set(genlist, "itc", itc);
+	evas_object_data_set(genlist, "appdata", ad);
 	sprintf(query, "select * from versewidget;");
 	_app_database_query(query, &_get_verse, genlist);
 	elm_layout_content_set(layout, "elm.swallow.content", genlist);
